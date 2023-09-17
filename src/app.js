e6d99370ba3d7c9a5bd2586434065808c0df744d
Victoria Kiseleva <castieeel@hotmail.com>
2023-09-17 20:59:00 +0800

    HW Done

--- src/app.js
+++ src/app.js
@@ -1,50 +1,48 @@
 const http = require("http");
+const fs = require("fs");
 const getUsers = require("./modules/users");
 
 const server = http.createServer((request, response) => {
-  const params = new URL(request.url);
-  const url = params.searchParams;
-  console.log(url);
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const params = url.searchParams;
 
   if (request.url === "/") {
     response.status = 200;
     response.statusMessage = "OK";
     response.header = "Content-Type: text/plain";
-    response.write("Hello, world");
+    response.write("Hello, world!");
     response.end();
 
     return;
   } else if (request.url === "/?users") {
     response.status = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+    response.header = "Content-Type: appliction/json";
     response.write(getUsers());
     response.end();
 
     return;
-  } else if (url.has("hello")) {
-    const name = url.get("hello");
-    console.log(name);
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
-    response.write(`Hello,${name}`);
-    response.end();
+  } else if (params.has("hello")) {
+    const name = params.get("hello");
+    if (name != "") {
+      response.status = 200;
+      response.statusMessage = "OK";
+      response.header = "Content-Type: text/plain";
+      response.write(`Hello, ${name}!`);
+      response.end();
+    } else {
+      response.status = 400;
+      response.statusMessage = "Error Bad Request";
+      response.header = "Content-Type: text/plain";
+      response.write("Enter a name");
+      response.end();
+    }
 
     return;
-    // } else if (request.url === "/?hello") {
-    //   response.status = 400;
-    //   response.statusMessage = "Bad Request";
-    //   response.header = "Content-Type: text/plain";
-    //   response.write("Enter a name");
-    //   response.end();
-
-    //   return;
   } else {
     response.status = 500;
     response.statusMessage = "Internal Server Error";
     response.header = "Content-Type: text/plain";
-    response.write("");
     response.end();
 
     return;
